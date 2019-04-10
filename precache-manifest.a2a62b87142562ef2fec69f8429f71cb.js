64e5afc62a91125d8f39ddd26ebae9f1c64ba823
charcole <charcole@pm.me>
2019-04-10 17:36:27 -0500

    Updates

---
+++ precache-manifest.a2a62b87142562ef2fec69f8429f71cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "16d04d7332e2fcb204f6",
+    "url": "/bloodborne-ally-app/static/js/runtime~main.a4e08c23.js"
+  },
+  {
+    "revision": "f221c546bdd892328166",
+    "url": "/bloodborne-ally-app/static/js/main.519919db.chunk.js"
+  },
+  {
+    "revision": "6e67350579d33906fa09",
+    "url": "/bloodborne-ally-app/static/js/2.835614ba.chunk.js"
+  },
+  {
+    "revision": "f221c546bdd892328166",
+    "url": "/bloodborne-ally-app/static/css/main.91705644.chunk.css"
+  },
+  {
+    "revision": "5273a478d8affef00d496a70165ad4cc",
+    "url": "/bloodborne-ally-app/index.html"
+  }
+];
